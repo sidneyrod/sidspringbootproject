95f9e3446f4ad00e3aada23238f1ffd6b390e1a4
Sidney Rodrigues <sidney.silva.rodrigues@outlook.com>
2021-02-26 11:54:14 -0300

    Project created

---
+++ src/main/java/com/siddev/sidproject/SidprojectApplication.java
@@ -0,0 +1,13 @@
+package com.siddev.sidproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SidprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SidprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/siddev/sidproject/SidprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siddev.sidproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SidprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
