1746175c989dc4594fc8f233654ecdc82116e044
Sidney Rodrigues <sidney.silva.rodrigues@outlook.com>
2021-03-02 14:10:16 -0300

    Exception handling - findById

--- src/main/java/com/siddev/sidproject/services/UserService.java
+++ src/main/java/com/siddev/sidproject/services/UserService.java
@@ -8,41 +8,42 @@ import org.springframework.stereotype.Service;
 
 import com.siddev.sidproject.entities.User;
 import com.siddev.sidproject.respositories.UserRepository;
+import com.siddev.sidproject.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
 
 	@Autowired
 	private UserRepository repository;
-	
+
 	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
-	
+
 	public User insert(User obj) {
 		return repository.save(obj);
 	}
-	
+
 	public void delete(Long id) {
 		repository.deleteById(id);
 	}
-	
+
 	public User update(Long id, User obj) {
 		User entity = repository.getOne(id);
 		updateData(entity, obj);
 		return repository.save(entity);
-		
+
 	}
 
 	private void updateData(User entity, User obj) {
 		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
 		entity.setPhone(obj.getPhone());
-		
+
 	}
 }
